5a4fcd6414bae8d5523d33c5b0a3b99b14561f8b
Britiney <britineyjohnson@gmail.com>
2023-10-08 20:45:56 -0400

    uploaded saving function

--- src/Main.java
+++ src/Main.java
@@ -1,218 +1,334 @@
-import java.util.LinkedList;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.runtime.SwitchBootstraps;
 import java.util.Scanner;
+
 public class Main {
 
-
     public static void main(String[] args) {
-   
+
         Baby b1 = new Baby();
         Child c1 = new Child();
         Teen t1 = new Teen();
         Adult a1 = new Adult();
         Old o1 = new Old();
-        
-        
-        RunGame(b1,c1,t1,a1,o1);
-        
+        Person p1 = new Person();
+
+        RunGame(b1, c1, t1, a1, o1, p1);
+
     }
 
+    public static void RunGame(Baby b1, Child c1, Teen t1, Adult a1, Old o1, Person p1) {
+        Scanner scan = new Scanner(System.in);
+        boolean load = false;
 
-    public static void RunGame(Baby b1, Child c1, Teen t1, Adult a1, Old o1){
-      Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to Load the last save?  (y/n)");
+        String ans = scan.nextLine();
 
-        System.out.println("Whats your name? :)");
-        b1.setName(scan.next());
+        if (ans.equals("y") || ans.equals("Y")) {
+            p1 = p1.loader(p1);
+            load = true;
 
+        } else {
+            File Person = new File("Person.XML");
+            Person.delete();
 
-        do {
-            b1.energyLimit();
-            b1.moneyLimit();
-            b1.moodLimit();
+            System.out.println("Whats your name? :)");
+            b1.setName(scan.nextLine());
+            p1.setAge("baby");
 
-            System.out.println("\nAge: Baby     " + b1.display());
-            System.out.println("Energy: "+b1.getEnergy()+"       1.Eat         4.Poop");
-            System.out.println("Money:  "+b1.getMoney()+"       2.Sleep");
-            System.out.println("Mood:   "+b1.getMood()+"       3.Cry");
-
-            System.out.println("How would you like to spend your time?");
-            int choice = scan.nextInt();
-
-            switch (choice){
-                case 1: b1.eat();
-                    break;
-
-                case 2: b1.sleep();
-                    break;
-
-                case 3: b1.cry();
-                    break;
-
-                case 4: b1.poop();
-                    break;
-
-                case 5: b1.test();
-                    break;
-            }
         }
-        while (b1.time < 7);
 
-        System.out.println("\n\nYou've grown!");
-        c1.time = 0;
-        c1.setName(b1.getName());
+        switch (p1.getAge()) {
+            case "baby":
+                do {
 
-        do {
-            c1.energyLimit();
-            c1.moneyLimit();
-            c1.moodLimit();
+                    b1.energyLimit();
+                    b1.moneyLimit();
+                    b1.moodLimit();
+                    b1.setAge("baby");
+                    if (load) {
+                        b1.setAge(p1.getAge());
+                        b1.setEnergy(p1.getEnergy());
+                        b1.setMoney(p1.getMoney());
+                        b1.setMood(p1.getMood());
+                        b1.setName(p1.getName());
+                        load = false;
+                    }
 
-            System.out.println("\nAge: Child     " + c1.display());
-            System.out.println("Energy: "+c1.getEnergy()+"       1.Eat         4.Play");
-            System.out.println("Money:  "+c1.getMoney()+"       2.Sleep");
-            System.out.println("Mood:   "+c1.getMood()+"       3.Cry");
+                    System.out.println("Age: Baby     " + b1.display());
+                    System.out.println("Energy: " + b1.getEnergy() + "       1.Eat         4.Poop");
+                    System.out.println("Money:  " + b1.getMoney() + "       2.Sleep       5.Save");
+                    System.out.println("Mood:   " + b1.getMood() + "       3.Cry");
 
-            System.out.println("How would you like to spend your time?");
-            int choice = scan.nextInt();
+                    System.out.println("How would you like to spend your time?");
+                    int choice = scan.nextInt();
 
-            switch (choice){
-                case 1: c1.eat();
-                    break;
+                    switch (choice) {
+                        case 1:
+                            b1.eat();
+                            break;
 
-                case 2: c1.sleep();
-                    break;
+                        case 2:
+                            b1.sleep();
+                            break;
 
-                case 3: c1.cry();
-                    break;
+                        case 3:
+                            b1.cry();
+                            break;
 
-                case 4: c1.play();
-                    break;
+                        case 4:
+                            b1.poop();
+                            break;
+                        case 5:
+                            b1.Save(b1);
+                            System.out.println("Game saved :)");
+                            break;
 
-                case 5: c1.test();
-                    break;
-            }
+                        case 7:
+                            b1.test();
+                            break;
+                    }
+                } while (b1.time < 7);
+
+                p1.setAge("child");
+            case "child":
+                System.out.println("\n\nYou've grown!");
+                c1.time = 0;
+                c1.setName(b1.getName());
+                if (load) {
+                    c1.setAge(p1.getAge());
+                    c1.setEnergy(p1.getEnergy());
+                    c1.setMoney(p1.getMoney());
+                    c1.setMood(p1.getMood());
+                    c1.setName(p1.getName());
+                    load = false;
+                }
+
+                do {
+                    c1.energyLimit();
+                    c1.moneyLimit();
+                    c1.moodLimit();
+                    c1.setAge("child");
+
+                    System.out.println("Age: Child     " + c1.display());
+                    System.out.println("Energy: " + c1.getEnergy() + "       1.Eat         4.Play");
+                    System.out.println("Money:  " + c1.getMoney() + "       2.Sleep       5.Save");
+                    System.out.println("Mood:   " + c1.getMood() + "       3.Cry");
+
+                    System.out.println("How would you like to spend your time?");
+                    int choice = scan.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            c1.eat();
+                            break;
+
+                        case 2:
+                            c1.sleep();
+                            break;
+
+                        case 3:
+                            c1.cry();
+                            break;
+
+                        case 4:
+                            c1.play();
+                            break;
+
+                        case 5:
+                            c1.Save(c1);
+                            System.out.println("Game saved :)");
+                            break;
+
+                        case 7:
+                            c1.test();
+                            break;
+                    }
+                } while (c1.time < 7);
+
+                p1.setAge("teen");
+            case "teen":
+                System.out.println("\n\nYou're getting bigger already");
+                t1.time = 0;
+                t1.setName(c1.getName());
+                if (load) {
+                    t1.setAge(p1.getAge());
+                    t1.setEnergy(p1.getEnergy());
+                    t1.setMoney(p1.getMoney());
+                    t1.setMood(p1.getMood());
+                    t1.setName(p1.getName());
+                    load = false;
+                }
+
+                do {
+                    t1.energyLimit();
+                    t1.moneyLimit();
+                    t1.moodLimit();
+                    t1.setAge("teen");
+
+                    System.out.println("Age: Teen     " + t1.display());
+                    System.out.println("Energy: " + t1.getEnergy() + "       1.Eat          4.Work");
+                    System.out.println("Money:  " + t1.getMoney() + "       2.Sleep        5.Shop");
+                    System.out.println("Mood:   " + t1.getMood() + "       3.Study        6.Save");
+
+                    System.out.println("How would you like to spend your time?");
+                    int choice = scan.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            t1.eat();
+                            break;
+
+                        case 2:
+                            t1.sleep();
+                            break;
+
+                        case 3:
+                            t1.study();
+                            break;
+
+                        case 4:
+                            t1.work();
+                            break;
+
+                        case 5:
+                            t1.shop();
+                            break;
+
+                        case 6:
+                            t1.Save(t1);
+                            System.out.println("Game saved :)");
+                            break;
+                        case 7:
+                            t1.test();
+                            break;
+                    }
+                } while (t1.time < 7);
+                p1.setAge("adult");
+            case "adult":
+                System.out.println("\n\nBoss makes a dollar I make a dime");
+                a1.time = 0;
+                a1.setName(c1.getName());
+                if (load) {
+                    a1.setAge(p1.getAge());
+                    a1.setEnergy(p1.getEnergy());
+                    a1.setMoney(p1.getMoney());
+                    a1.setMood(p1.getMood());
+                    a1.setName(p1.getName());
+                    load = false;
+                }
+
+                do {
+                    a1.energyLimit();
+                    a1.moneyLimit();
+                    a1.moodLimit();
+                    a1.setAge("adult");
+
+                    System.out.println("Age: Adult    " + a1.display());
+                    System.out.println("Energy: " + a1.getEnergy() + "       1.Eat          4.Work");
+                    System.out.println("Money:  " + a1.getMoney() + "       2.Sleep        5.Shop");
+                    System.out.println("Mood:   " + a1.getMood() + "       3.hobby        6.Save");
+
+                    System.out.println("How would you like to spend your time?");
+                    int choice = scan.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            a1.eat();
+                            break;
+
+                        case 2:
+                            a1.sleep();
+                            break;
+
+                        case 3:
+                            a1.hobby();
+                            break;
+
+                        case 4:
+                            a1.work();
+                            break;
+
+                        case 5:
+                            a1.shop();
+                            break;
+
+                        case 6:
+                            a1.Save(a1);
+                            System.out.println("Game saved :)");
+                            break;
+                        case 7:
+                            a1.test();
+                            break;
+                    }
+                } while (a1.time < 7);
+                p1.setAge("old");
+            case "old":
+                System.out.println("\n\nOOHH, my back!");
+                o1.time = 0;
+                o1.setName(c1.getName());
+                if (load) {
+                    o1.setAge(p1.getAge());
+                    o1.setEnergy(p1.getEnergy());
+                    o1.setMoney(p1.getMoney());
+                    o1.setMood(p1.getMood());
+                    o1.setName(p1.getName());
+                    load = false;
+                }
+
+                do {
+                    o1.energyLimit();
+                    o1.moneyLimit();
+                    o1.moodLimit();
+                    o1.setAge("old");
+
+                    System.out.println("Age: Old      " + o1.display());
+                    System.out.println("Energy: " + o1.getEnergy() + "       1.Eat          4.tv");
+                    System.out.println("Money:  " + o1.getMoney() + "       2.Sleep        5.Shop");
+                    System.out.println("Mood:   " + o1.getMood() + "       3.knit         6.Save");
+
+                    System.out.println("How would you like to spend your time?");
+                    int choice = scan.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            o1.eat();
+                            break;
+
+                        case 2:
+                            o1.sleep();
+                            break;
+
+                        case 3:
+                            o1.knit();
+                            break;
+
+                        case 4:
+                            o1.tv();
+                            break;
+
+                        case 5:
+                            o1.shop();
+                            break;
+
+                        case 6:
+                            o1.Save(o1);
+                            System.out.println("Game saved :)");
+                            break;
+
+                        case 7:
+                            o1.test();
+                            break;
+                    }
+                } while (o1.time < 7);
+
         }
-        while (c1.time < 7);
-
-        System.out.println("\n\nYou're getting bigger already");
-        t1.time = 0;
-        t1.setName(c1.getName());
-
-
-
-        do {
-            t1.energyLimit();
-            t1.moneyLimit();
-            t1.moodLimit();
-
-            System.out.println("\nAge: Teen     " + t1.display());
-            System.out.println("Energy: "+t1.getEnergy()+"       1.Eat          4.Work");
-            System.out.println("Money:  "+t1.getMoney()+"       2.Sleep        5.Shop");
-            System.out.println("Mood:   "+t1.getMood()+"       3.Study");
-
-            System.out.println("How would you like to spend your time?");
-            int choice = scan.nextInt();
-
-            switch (choice){
-                case 1: t1.eat();
-                    break;
-
-                case 2: t1.sleep();
-                    break;
-
-                case 3: t1.study();
-                    break;
-
-                case 4: t1.work();
-                    break;
-
-                case 5: t1.shop();
-                    break;
-                case 6: t1.test();
-                    break;
-            }
-        }
-        while (t1.time < 7);
-
-        System.out.println("\n\nBoss makes a dollar I make a dime");
-        Adult.time=0;
-        a1.setName(c1.getName());
-
-
-        do {
-            a1.energyLimit();
-            a1.moneyLimit();
-            a1.moodLimit();
-
-            System.out.println("\nAge: Adult    " + a1.display());
-            System.out.println("Energy: "+a1.getEnergy()+"       1.Eat          4.Work");
-            System.out.println("Money:  "+a1.getMoney()+"       2.Sleep        5.Shop");
-            System.out.println("Mood:   "+a1.getMood()+"       3.hobby");
-
-            System.out.println("How would you like to spend your time?");
-            int choice = scan.nextInt();
-
-            switch (choice){
-                case 1: a1.eat();
-                    break;
-
-                case 2: a1.sleep();
-                    break;
-
-                case 3: a1.hobby();
-                    break;
-
-                case 4: a1.work();
-                    break;
-
-                case 5: a1.shop();
-                    break;
-                case 6: a1.test();
-                    break;
-            }
-        }
-        while (a1.time < 7);
-
-        System.out.println("\n\nOOHH, my back!");
-        Old.time = 0;
-        o1.setName(c1.getName());
-
-        do {
-            o1.energyLimit();
-            o1.moneyLimit();
-            o1.moodLimit();
-
-            System.out.println("\nAge: Old      " + o1.display());
-            System.out.println("Energy: "+o1.getEnergy()+"       1.Eat          4.tv");
-            System.out.println("Money:  "+o1.getMoney()+"       2.Sleep        5.Shop");
-            System.out.println("Mood:   "+o1.getMood()+"       3.knit");
-
-            System.out.println("How would you like to spend your time?");
-            int choice = scan.nextInt();
-
-            switch (choice){
-                case 1: o1.eat();
-                    break;
-
-                case 2: o1.sleep();
-                    break;
-
-                case 3: o1.knit();
-                    break;
-
-                case 4: o1.tv();
-                    break;
-
-                case 5: o1.shop();
-                    break;
-                case 6: o1.test();
-                    break;
-            }
-        }
-        while (o1.time < 7);
-
-        for (int i =5; i>0; i--){
+        for (int i = 5; i > 0; i--) {
             System.out.println("---------------");
         }
         System.out.println("Well its over now");
+        scan.close();
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -1,71 +1,89 @@
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Person {
     private String name;
+    public String age;
     private int energy;
     private int mood;
     private int money;
-    public static double time = 0;
+    public double time = 0;
 
-    public void test(){
+    public void test() {
         time += 7;
     }
 
-    public Person(){
-       energy = 0;
-       mood = 0;
-       money = 0;
-       this.name = "";
+    public Person() {
+        energy = 0;
+        mood = 0;
+        money = 0;
+        this.name = "";
+        this.age = "";
 
     }
 
-    public void energyLimit(){
-        if (energy > 20 ){energy = 20;}
-        if (energy < 0){energy = 0;}
-    }
-    public void moodLimit(){
-        if (mood > 20){mood = 20;}
-        if (mood < 0){mood = 0;}
-    }
-    public void moneyLimit(){
-        if (money< 0) money = 0;
+    public void energyLimit() {
+        if (energy > 20) {
+            energy = 20;
+        }
+        if (energy < 0) {
+            energy = 0;
+        }
     }
 
-    public void AddEnergy(int AddE){
+    public void moodLimit() {
+        if (mood > 20) {
+            mood = 20;
+        }
+        if (mood < 0) {
+            mood = 0;
+        }
+    }
+
+    public void moneyLimit() {
+        if (money < 0)
+            money = 0;
+    }
+
+    public void AddEnergy(int AddE) {
         energy += AddE;
     }
-    public void SubEnergy(int SubE){
+
+    public void SubEnergy(int SubE) {
         energy -= SubE;
     }
 
-
-    public void AddMood(int AddMood){
+    public void AddMood(int AddMood) {
         mood += AddMood;
     }
-    public void SubMood(int SubMood){
+
+    public void SubMood(int SubMood) {
         mood -= SubMood;
     }
 
-
-    public void AddMoney(int AddMoney){
+    public void AddMoney(int AddMoney) {
         money += AddMoney;
     }
-    public void SubMoney(int SubMoney){
+
+    public void SubMoney(int SubMoney) {
         money += SubMoney;
     }
 
-
-    public void eat(){
+    public void eat() {
         AddEnergy(5);
         AddMood(5);
         time += .25;
     }
 
-    public void sleep(){
+    public void sleep() {
         AddEnergy(5);
         AddMood(5);
         time += .4;
     }
-
-
 
     public String getName() {
         return name;
@@ -81,6 +99,14 @@ public class Person {
 
     public void setEnergy(int energy) {
         this.energy = energy;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 
     public int getMood() {
@@ -99,15 +125,39 @@ public class Person {
         this.money = money;
     }
 
+    public void Save(Person pp) {
+        try {
+            FileOutputStream fos = new FileOutputStream(new File("./Person.xml"));
+            XMLEncoder enc = new XMLEncoder(fos);
+            enc.writeObject(pp);
+            enc.close();
+            fos.close();
 
-
-    public String display(){
-        return "Day: "+time+"    Name: "+getName();
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    public Person loader(Person pp) {
+        try {
+            FileInputStream fis = new FileInputStream(new File("./Person.xml"));
+            XMLDecoder Dec = new XMLDecoder(fis);
 
-    
+            Person NewGal = (Person) Dec.readObject();
+            Dec.close();
+            fis.close();
 
+            pp = NewGal;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pp;
+    }
+
+    public String display() {
+        return "Day: " + time + "    Name: " + getName();
+
+    }
 
 }
